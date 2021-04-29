28d090913f3b410f09cfa72b176b44eeafd423f6
Andrey <andreysidor4uk@yandex.ru>
2021-04-29 16:32:24 +0300

    Added timeout in send_message

--- delidog/bot.py
+++ delidog/bot.py
@@ -26,7 +26,7 @@ def _set_token(message):
 
 def send_message(chat, text, disable_notification=False):
     bot.send_message(
-        chat.id, text, disable_notification=disable_notification)
+        chat.id, text, disable_notification=disable_notification, timeout=5)
     Message.add_message(
         chat,
         text,
